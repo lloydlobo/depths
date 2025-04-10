9e2f87424e75be1195418d15598955821e096c06
Lloyd Lobo <lloydlobo4@gmail.com>
2025-04-10 17:49:17 +0530

    feat: implement player velocity and air timer for jumps

--- main.go
+++ main.go
@@ -22,8 +22,8 @@ func main() {
 	rl.InitWindow(screenWidth, screenHeight, "raylib [models] example - box collisions")
 	rl.ToggleFullscreen()
 
-	const arenaWidth = float32(20)  // X
-	const arenaLength = float32(20) // Z
+	const arenaWidth = float32(10) * 3  // X
+	const arenaLength = float32(10) * 3 // Z
 
 	camera := rl.Camera{}
 	camera.Position = rl.NewVector3(0.0, arenaWidth, arenaLength)
@@ -36,9 +36,25 @@ func main() {
 	sphereMesh := rl.GenMeshSphere(sphereModelRadius, 16, 16)
 	sphereModel := rl.LoadModelFromMesh(sphereMesh)
 
+	isPlayerBoost := false
+	isPlayerStrafe := false
+	_ = isPlayerBoost
+	_ = isPlayerStrafe
+	playerColor := rl.RayWhite
+	playerJumpsLeft := 1
 	playerPosition := rl.NewVector3(0.0, 1.0, 2.0)
 	playerSize := rl.NewVector3(1.0, 2.0, 1.0)
-	playerColor := rl.RayWhite
+	playerVelocity := rl.Vector3{}
+	playerAirTimer := float32(0)
+
+	maxPlayerAirTime := 5 * float32(fps)
+	const movementMagnitude = float32(0.2)
+	const playerJumpVelocity = 3
+	const terminalVelocityY = 5
+	// # if max: 0.1333333333.. (makes jumping possible to 3x player height) # else use min for easy floaty feel
+	// self._terminal_limiter_air_friction: Final = max(0.1, ((pre.TILE_SIZE * 0.5) / (pre.FPS_CAP)))
+	const terminalVelocityLimiterAirFriction = movementMagnitude / math.Phi
+	const terminalVelocityLimiterAirFrictionY = movementMagnitude / 2
 
 	// See also https://github.com/Pakz001/Raylib-Examples/blob/master/ai/Example_-_Pattern_Movement.c
 	enemyBoxPos := rl.NewVector3(-4.0, 1.0, 0.0)
@@ -64,9 +80,44 @@ func main() {
 
 	framesCounter := 0
 
+	handlePlayerJump := func() {
+		playerJumpsLeft--
+		playerVelocity.Y = playerJumpVelocity
+		playerAirTimer = maxPlayerAirTime
+	}
+
 	rl.SetTargetFPS(fps)
 
 	for !rl.WindowShouldClose() {
+		// Handle user input events
+
+		playerMovementThisFrame := rl.Vector3{}
+		playerCollisionsThisFrame := rl.Vector4{}
+
+		if rl.IsKeyDown(rl.KeyRight) {
+			playerMovementThisFrame.X += 1 // Right
+		}
+		if rl.IsKeyDown(rl.KeyLeft) {
+			playerMovementThisFrame.X -= 1 // Left
+		}
+		if rl.IsKeyDown(rl.KeyDown) {
+			playerMovementThisFrame.Z += 1 // Backward
+		}
+		if rl.IsKeyDown(rl.KeyUp) {
+			playerMovementThisFrame.Z -= 1 // Forward
+		}
+		if rl.IsKeyDown(rl.KeyLeftShift) {
+			isPlayerBoost = true
+		}
+		if rl.IsKeyDown(rl.KeyLeftAlt) {
+			isPlayerStrafe = true
+		}
+		if rl.IsKeyDown(rl.KeySpace) { // Jump
+			if playerJumpsLeft > 0 {
+				handlePlayerJump()
+			}
+		}
+
 		// Update
 
 		// Store previous position to reuse as next postion on collision
@@ -74,57 +125,92 @@ func main() {
 		oldCamPos := camera.Position
 		_ = oldCamPos
 
-		// Move player
-		const magnitude = float32(0.2)
-		currMagnitude := magnitude
-		isBoost := false
-		isStrafe := false
-		movement := rl.Vector3Zero()
-		if rl.IsKeyDown(rl.KeyRight) {
-			movement.X += 1 // Right
-		}
-		if rl.IsKeyDown(rl.KeyLeft) {
-			movement.X -= 1 // Left
-		}
-		if rl.IsKeyDown(rl.KeyDown) {
-			movement.Z += 1 // Backward
-		}
-		if rl.IsKeyDown(rl.KeyUp) {
-			movement.Z -= 1 // Forward
-		}
-		if isMoveYPlane := true; isMoveYPlane {
-			if rl.IsKeyDown(rl.KeySpace) {
-				movement.Y += 1 // Up
-				currMagnitude *= math.Phi * math.Phi
-			}
-			if rl.IsKeyDown(rl.KeyLeftControl) {
-				movement.Y -= 1 // Down
-			}
-		}
-		if rl.IsKeyDown(rl.KeyLeftShift) {
-			isBoost = true
-		}
-		if rl.IsKeyDown(rl.KeyLeftAlt) {
-			isStrafe = true
-		}
-		if !rl.Vector3Equals(movement, rl.Vector3Zero()) { // Vector3Length (XZ): 1.414 --diagonal-> 0.99999994
-			movement = rl.Vector3Normalize(movement) // See also https://community.monogame.net/t/how-can-i-normalize-my-diagonal-movement/15276
-		}
-		if isBoost {
-			currMagnitude *= math.Phi
-		}
-		if isStrafe {
-			currMagnitude /= math.Phi
-		}
-		playerPosition.X += movement.X * currMagnitude
-		playerPosition.Y += movement.Y * currMagnitude
-		playerPosition.Z += movement.Z * currMagnitude
+		// Update player
+		{
+			playerAirTimer++
+			// currMagnitude := movementMagnitude
 
-		// Apply Gravity
-		playerPosition.Y -= magnitude * (math.Phi / 2)
-		// HACK: Check if player is touching an infinite floor
-		if playerPosition.Y+playerSize.Y/2 < 2 {
-			playerPosition.Y = playerSize.Y / 2
+			// Normalize input vector to avoid speeding up diagonally
+			if !rl.Vector3Equals(playerMovementThisFrame, rl.Vector3Zero()) { // Vector3Length (XZ): 1.414 --diagonal-> 0.99999994
+				playerMovementThisFrame = rl.Vector3Normalize(playerMovementThisFrame) // See also https://community.monogame.net/t/how-can-i-normalize-my-diagonal-movement/15276
+			}
+
+			// Copied from https://github.com/lloydlobo/tiptoe/blob/master/src/internal/entities.py
+			// """
+			// Update the entity's position based on physics and collisions.
+			// Physics: movement via collision detection 2 part axis method handle one
+			// axis at a time for predictable resolution also pygame-ce allows
+			// calculating floats with Rects
+			// Note: For each X and Y axis movement, we update x and y position as int
+			// as pygame rect don't handle it as of now.
+			// """
+			// # Compute players input based movement with entity velocity
+			// frame_movement: pg.Vector2 = movement + self.velocity
+			frameMovement := rl.Vector3Add(playerMovementThisFrame, playerVelocity)
+			{
+				playerPosition.X += frameMovement.X * movementMagnitude
+				if isTouchXPlaneEdges := playerPosition.X-playerSize.X/2 < -arenaWidth/2 ||
+					playerPosition.X+playerSize.X/2 > arenaWidth/2; isTouchXPlaneEdges {
+					playerCollisionsThisFrame.X = 1
+				}
+				playerPosition.Y += frameMovement.Y * movementMagnitude
+				if false {
+					playerCollisionsThisFrame.Y = 1
+				}
+				playerPosition.Z += frameMovement.Z * movementMagnitude
+				if isTouchZPlaneEdges := playerPosition.Z-playerSize.Z/2 < -arenaLength/2 ||
+					playerPosition.Z+playerSize.Z/2 > arenaLength/2; isTouchZPlaneEdges {
+					playerCollisionsThisFrame.Z = 1
+				}
+
+				// HACK: Gravity: Check if player is touching an infinite floor
+				isTouchFloor := playerPosition.Y+playerSize.Y/2 < 2
+				if isTouchFloor {
+					playerCollisionsThisFrame.W = 1
+				}
+
+				// # Entity: Update velocity
+				playerVelocity.Y = MinF(terminalVelocityY, playerVelocity.Y-terminalVelocityLimiterAirFrictionY) // Decelerate if in air
+
+				// # Entity: Handle velocity based on collisions up or down
+				if playerCollisionsThisFrame.Y == 1 || playerCollisionsThisFrame.W == 1 {
+					playerVelocity.Y = 0 // self.Velocity = 0
+				}
+
+				// # Entity:Player: Handle velocity based on collisions
+				if playerCollisionsThisFrame.Y == 1 || playerCollisionsThisFrame.W == 1 {
+					// ... or rl.QuaternionLength(playerCollisionsThisFrame) > 0
+					playerAirTimer = 0
+					playerPosition.Y = playerSize.Y / 2 // Fix to ground
+					playerJumpsLeft = 1
+				}
+
+				if playerAirTimer > maxPlayerAirTime {
+					// fmt.Printf("playerAirTimer: %v\n", playerAirTimer)
+					// panic(playerAirTimer)
+					// playerVelocity.Y -= terminalVelocityLimiterAirFrictionY
+				}
+
+				if maxAirTimeToGameOver := maxPlayerAirTime * float32(fps); playerAirTimer > maxAirTimeToGameOver {
+					panic("Unimplemented: playerAirTimer > maxAirTimeToGameOver")
+				}
+			}
+
+			// Apply Gravity
+			playerVelocity.Y -= terminalVelocityLimiterAirFrictionY
+
+			// Normalize velocity along XZ plane (width and length)
+			// Only for player (remove for other entities)!!!!!
+			if playerVelocity.X > 0 {
+				playerVelocity.X = MaxF(0, playerVelocity.X-terminalVelocityLimiterAirFriction)
+			} else {
+				playerVelocity.X = MinF(0, playerVelocity.X+terminalVelocityLimiterAirFriction)
+			}
+			if playerVelocity.Z > 0 {
+				playerVelocity.Z = MaxF(0, playerVelocity.Z-terminalVelocityLimiterAirFriction)
+			} else {
+				playerVelocity.Z = MinF(0, playerVelocity.Z+terminalVelocityLimiterAirFriction)
+			}
 		}
 		// camera.Position.Z = rl.Lerp(camera.Position.Z, arenaLength, math.Phi-1)
 		// camera.Position.Z = rl.Lerp(camera.Position.Z, playerPosition.Z, math.Phi-1)
@@ -259,3 +345,15 @@ func main() {
 
 	rl.CloseWindow()
 }
+
+// From cmp.Ordered
+type OrderedNumber interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64 |
+		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr |
+		~float32 | ~float64
+}
+type NumberType OrderedNumber
+
+func MaxF[T NumberType](x T, y T) float32 { return float32(max(float64(x), float64(y))) }
+func MinF[T NumberType](x T, y T) float32 { return float32(min(float64(x), float64(y))) }
+
